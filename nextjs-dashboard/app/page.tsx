63748b5e29d5e45f58fb2c0406ce101c17a655e4
Phat Hoang <hoangphat4510@gmail.com>
2024-10-29 10:00:49 +0700

    second commit

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -1,9 +1,9 @@
-import AcmeLogo from '@/app/ui/acme-logo';
-import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import AcmeLogo from "@/app/ui/acme-logo";
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import styles from "@/app/ui/home.module.css";
-import { lusitana } from './ui/fonts';
-import Link from 'next/link';
-import Image from 'next/image';
+import { lusitana } from "./ui/fonts";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Page() {
   return (
@@ -14,8 +14,10 @@ export default function Page() {
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={` ${lusitana.className} antialiased text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+          <p
+            className={` ${lusitana.className} antialiased text-xl text-gray-800 md:text-3xl md:leading-normal`}
+          >
+            <strong>Welcome to Acme.</strong> This is the example for the{" "}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
@@ -29,16 +31,20 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          <Image src="/hero-desktop.png"
+          <Image
+            src="/hero-desktop.png"
             width={1000}
             height={760}
             className="hidden md:block"
-            alt="Screenshots of the dashboard project showing desktop version" />
-          <Image src="/hero-mobile.png"
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+          <Image
+            src="/hero-mobile.png"
             width={560}
             height={620}
             className="md:hidden"
-            alt="Screenshots of the dashboard project showing desktop version" />
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
         </div>
       </div>
     </main>
